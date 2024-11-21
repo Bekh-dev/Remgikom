52d36856274a36fae7f2e767dc5238b61290b6a4
Bekh <isbek2000@gmail.com>
2024-11-21 15:10:01 +0500

    Added footer block, implemented scrollTop function

--- #sourse/js/main.js
+++ #sourse/js/main.js
@@ -1,6 +1,22 @@
 //==========================================================================
 "use strict"
 
+
+
+window.addEventListener('DOMContentLoaded', () => {
+
+	const btnTop = document.querySelector('.footer__back-top');
+
+	btnTop.addEventListener('click', (e) => {
+		e.preventDefault();
+		window.scrollTo({
+			top: 0,
+			behavior: 'smooth'
+		});
+	});
+
+});
+
 //Footer-Scroll-Top==========================================================
 
 //Spoller======================================================================
--- js/main.js
+++ js/main.js
@@ -1,1 +1,1 @@
-"use strict";
+"use strict";window.addEventListener("DOMContentLoaded",(()=>{document.querySelector(".footer__back-top").addEventListener("click",(e=>{e.preventDefault(),window.scrollTo({top:0,behavior:"smooth"})}))}));
